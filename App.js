f2cc1b51ee24069ca9af3ae551a49f6e29e1b2de
Solomon Amsalu <solamsalu@gmail.com>
2022-02-12 06:52:35 +0300

    this is react project

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from '../src/Components/Header/Header'
+import Alert from './Components/Main/Alert'
+import FirstSection from './Components/Main/FirstSection'
+import SecondSection from './Components/Main/SecondSection'
+import ThirdSection from './Components/Main/ThirdSection'
+import FourthSection from './Components/Main/FourthSection'
+import FifthSection from './Components/Main/FifthSection'
+
+
+import Footer from './Components/Footer/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Alert />
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <FourthSection />
+      <FifthSection />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
